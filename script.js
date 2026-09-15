ad8cee15e51b36bf2296d2df3289b3e1300d1053
arthursanchezcorrea-jpg <arthur.sanchez.correa@escola.pr.gov.br>
2026-09-15 11:17:39 -0300

    atualizei

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ botoesCurtir.forEach(function(botaoCurtir){
     let curtiu = false;
     botaoCurtir.addEventListener("click",curtir);
 function curtir(){
-    const contdor = botaoCurtir.querySelector("span");
+    const contador = botaoCurtir.querySelector("span");
     if(curtiu === false){
         contador.textContent++;
         curtiu === true;}
